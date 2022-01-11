7cc4ecf467a53293f4c974fda3e39c8b6c1644ef
Adilson Pereira <adilsonpereiradesenvolvimento@gmail.com>
2022-01-10 21:44:41 -0300

    First commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,13 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>PostalCode</title>
+      </Head>
+
+      <h1>Postal Code</h1>
+    </>
+  );
+}
