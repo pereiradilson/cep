b0652b1d3caf4b876cdd9ea3da73283abc8080fc
Adilson Pereira <adilsonpereiradesenvolvimento@gmail.com>
2022-01-10 22:48:36 -0300

    Project in progress

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,7 +5,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head />
-        <body>
+        <body className="bg-background-dark">
           <Main />
           <NextScript />
         </body>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,37 @@
 import Head from "next/head";
+import Image from "next/image";
 
 export default function Home() {
   return (
     <>
       <Head>
-        <title>PostalCode</title>
+        <title>CEP | Código de Endereçamento Postal</title>
       </Head>
 
-      <h1>Postal Code</h1>
+      <div className="container mx-auto">
+        <div className="flex flex-col justify-center items-center">
+          <div className="m-5">
+            <Image
+              src="/logo-cep-dark.png"
+              width="350"
+              height="150"
+              alt="Cep"
+            />
+          </div>
+
+          <div className="flex flex-row m-5">
+            <input
+              type="text"
+              name="cep"
+              placeholder="Informe o cep"
+              className="p-3 w-full border-2 border-gray-600 rounded-lg focus:outline-none focus:border-gray-700"
+              maxLength={9}
+            />
+
+            <button onClick={() => {}}>Pesquisar</button>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
